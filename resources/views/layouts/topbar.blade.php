9d9559d1a2a7da4ebebf35d840fef7db0ec3f245
lucaslopeslima95 <lucaslopesllima@gmail.com>
2023-12-30 18:51:41 -0300

    Ajustes de layout

--- resources/views/layouts/topbar.blade.php
+++ resources/views/layouts/topbar.blade.php
@@ -2,23 +2,6 @@
   <div class="container-fluid px-0">
     <div class="d-flex justify-content-between w-100" id="navbarSupportedContent">
       <div class="d-flex align-items-center">
-        <!-- Search form -->
-        <form class="navbar-search form-inline" id="navbar-search-main">
-          <div class="input-group input-group-merge search-bar">
-            <span class="input-group-text" id="topbar-addon"><svg class="icon icon-xs"
-                x-description="Heroicon name: solid/search" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
-                fill="currentColor" aria-hidden="true">
-                <path fill-rule="evenodd"
-                  d="M8 4a4 4 0 100 8 4 4 0 000-8zM2 8a6 6 0 1110.89 3.476l4.817 4.817a1 1 0 01-1.414 1.414l-4.816-4.816A6 6 0 012 8z"
-                  clip-rule="evenodd"></path>
-              </svg></span></span>
-            <input type="text" class="form-control" id="topbarInputIconLeft" placeholder="Search" aria-label="Search"
-              aria-describedby="topbar-addon">
-          </div>
-        </form>
-        <a href="https://themesberg.com/product/laravel/volt-admin-dashboard-template" target="_blank"><button
-            class="btn mx-1 me-2 btn-secondary" type="button"><i
-              class="fas fa-arrow-down mx-1"></i>Download</button></a>
       </div>
       <!-- Navbar links -->
       <ul class="navbar-nav align-items-center">
@@ -35,102 +18,6 @@
           <div class="dropdown-menu dropdown-menu-lg dropdown-menu-center mt-2 py-0">
             <div class="list-group list-group-flush">
               <a href="#" class="text-center text-primary fw-bold border-bottom border-light py-3">Notifications</a>
-              <a href="#" class="list-group-item list-group-item-action border-bottom">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <!-- Avatar -->
-                    <img alt="Image placeholder" src="/assets/img/team/profile-picture-1.jpg" class="avatar-md rounded">
-                  </div>
-                  <div class="col ps-0 ms-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div>
-                        <h4 class="h6 mb-0 text-small">Jose Leos</h4>
-                      </div>
-                      <div class="text-end">
-                        <small class="text-danger">a few moments ago</small>
-                      </div>
-                    </div>
-                    <p class="font-small mt-1 mb-0">Added you to an event "Project stand-up" tomorrow at 12:30 AM.</p>
-                  </div>
-                </div>
-              </a>
-              <a href="#" class="list-group-item list-group-item-action border-bottom">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <!-- Avatar -->
-                    <img alt="Image placeholder" src="/assets/img/team/profile-picture-2.jpg" class="avatar-md rounded">
-                  </div>
-                  <div class="col ps-0 ms-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div>
-                        <h4 class="h6 mb-0 text-small">Neil Sims</h4>
-                      </div>
-                      <div class="text-end">
-                        <small class="text-danger">2 hrs ago</small>
-                      </div>
-                    </div>
-                    <p class="font-small mt-1 mb-0">You've been assigned a task for "Awesome new project".</p>
-                  </div>
-                </div>
-              </a>
-              <a href="#" class="list-group-item list-group-item-action border-bottom">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <!-- Avatar -->
-                    <img alt="Image placeholder" src="/assets/img/team/profile-picture-3.jpg" class="avatar-md rounded">
-                  </div>
-                  <div class="col ps-0 m-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div>
-                        <h4 class="h6 mb-0 text-small">Roberta Casas</h4>
-                      </div>
-                      <div class="text-end">
-                        <small>5 hrs ago</small>
-                      </div>
-                    </div>
-                    <p class="font-small mt-1 mb-0">Tagged you in a document called "Financial plans",</p>
-                  </div>
-                </div>
-              </a>
-              <a href="#" class="list-group-item list-group-item-action border-bottom">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <!-- Avatar -->
-                    <img alt="Image placeholder" src="/assets/img/team/profile-picture-4.jpg" class="avatar-md rounded">
-                  </div>
-                  <div class="col ps-0 ms-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div>
-                        <h4 class="h6 mb-0 text-small">Joseph Garth</h4>
-                      </div>
-                      <div class="text-end">
-                        <small>1 d ago</small>
-                      </div>
-                    </div>
-                    <p class="font-small mt-1 mb-0">New message: "Hey, what's up? All set for the presentation?"</p>
-                  </div>
-                </div>
-              </a>
-              <a href="#" class="list-group-item list-group-item-action border-bottom">
-                <div class="row align-items-center">
-                  <div class="col-auto">
-                    <!-- Avatar -->
-                    <img alt="Image placeholder" src="/assets/img/team/profile-picture-5.jpg" class="avatar-md rounded">
-                  </div>
-                  <div class="col ps-0 ms-2">
-                    <div class="d-flex justify-content-between align-items-center">
-                      <div>
-                        <h4 class="h6 mb-0 text-small">Bonnie Green</h4>
-                      </div>
-                      <div class="text-end">
-                        <small>2 hrs ago</small>
-                      </div>
-                    </div>
-                    <p class="font-small mt-1 mb-0">New message: "We need to improve the UI/UX for the landing page."
-                    </p>
-                  </div>
-                </div>
-              </a>
               <a href="#" class="dropdown-item text-center fw-bold rounded-bottom py-3">
                 <svg class="icon icon-xxs text-gray-400 me-1" fill="currentColor" viewBox="0 0 20 20"
                   xmlns="http://www.w3.org/2000/svg">
@@ -151,7 +38,7 @@
               <img class="avatar rounded-circle" alt="Image placeholder" src="/assets/img/team/profile-picture-1.jpg">
               <div class="media-body ms-2 text-dark align-items-center d-none d-lg-block">
                 <span
-                  class="mb-0 font-small fw-bold text-gray-900">{{  auth()->user()->first_name ? auth()->user()->first_name . ' ' . auth()->user()->last_name : 'User Name'}}</span>
+                  class="mb-0 font-small fw-bold text-gray-900">{{  auth()->user()->name ? auth()->user()->name: 'Nome de Usuario'}}</span>
               </div>
             </div>
           </a>
@@ -163,7 +50,7 @@
                   d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-6-3a2 2 0 11-4 0 2 2 0 014 0zm-2 4a5 5 0 00-4.546 2.916A5.986 5.986 0 0010 16a5.986 5.986 0 004.546-2.084A5 5 0 0010 11z"
                   clip-rule="evenodd"></path>
               </svg>
-              My Profile
+              Meu Perfil 
             </a>
             <div role="separator" class="dropdown-divider my-1"></div>
             <a class="dropdown-item d-flex align-items-center">
