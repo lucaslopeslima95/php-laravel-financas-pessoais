8aef396e47d87264cb6b66f9bac9b453773d733b
lucaslopeslima95 <lucaslopesllima@gmail.com>
2024-01-14 16:36:28 -0300

    Adicionado login Com github

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Livewire\BootstrapTables;
 use App\Livewire\Components\Buttons;
 use App\Livewire\Components\Forms;
 use App\Livewire\Components\Modals;
@@ -16,27 +15,23 @@ use App\Livewire\Auth\Login;
 use App\Livewire\Profile;
 use App\Livewire\Auth\Register;
 use App\Livewire\ForgotPasswordExample;
-use App\Livewire\Index;
 use App\Livewire\LoginExample;
 use App\Livewire\ProfileExample;
 use App\Livewire\RegisterExample;
 use App\Livewire\Transactions;
 use Illuminate\Support\Facades\Route;
 use App\Livewire\ResetPasswordExample;
-use App\Livewire\UpgradeToPro;
 use App\Livewire\Users;
+use Laravel\Socialite\Facades\Socialite;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+Route::get('/auth/redirect', function () {
+    return Socialite::driver('github')->redirect();
+});
 
+Route::get('/auth/callback', function () {
+    $user = Socialite::driver('github')->user();
+    dd($user);
+});
 Route::redirect('/', '/login');
 
 Route::get('/register', Register::class)->name('register');
@@ -47,9 +42,11 @@ Route::get('/forgot-password', ForgotPassword::class)->name('forgot-password');
 
 Route::get('/reset-password/{id}', ResetPassword::class)->name('reset-password')->middleware('signed');
 
+
 Route::get('/404', Err404::class)->name('404');
 Route::get('/500', Err500::class)->name('500');
-Route::get('/upgrade-to-pro', UpgradeToPro::class)->name('upgrade-to-pro');
+
+
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', Profile::class)->name('profile');
@@ -61,7 +58,6 @@ Route::middleware('auth')->group(function () {
     Route::get('/reset-password-example', ResetPasswordExample::class)->name('reset-password-example');
     Route::get('/dashboard', Dashboard::class)->name('dashboard');
     Route::get('/transactions', Transactions::class)->name('transactions');
-    Route::get('/bootstrap-tables', BootstrapTables::class)->name('bootstrap-tables');
     Route::get('/lock', Lock::class)->name('lock');
     Route::get('/buttons', Buttons::class)->name('buttons');
     Route::get('/notifications', Notifications::class)->name('notifications');
@@ -69,3 +65,5 @@ Route::middleware('auth')->group(function () {
     Route::get('/modals', Modals::class)->name('modals');
     Route::get('/typography', Typography::class)->name('typography');
 });
+
+
